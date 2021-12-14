8b0dcb7acc862c80f61e5c1a7f16465692a1488f
Min Cho <minsangcho19@gmail.com>
2021-12-14 10:09:44 -0800

    commit tree 69669f28e52e6351422290974b0a4a86a1fcdca8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(def = 'roller-skate'){
+    return "This Saturday, I want to " + def + "!"
+}
+
+function mondayWork(def = 'go to the office'){
+    return "This Monday, I will " + def + "."
+}
+
+function wrapAdjective(flair){
+    function special(d = 'special'){
+        if (flair === '*') return 'You are ' + '*' + d + '*' + "!"
+        else if (flair === '||') return 'You are ' + '||' + d + '||' + "!"
+        return 'You are ' + d
+    }
+    return special
+}
